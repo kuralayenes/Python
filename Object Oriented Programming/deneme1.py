31a64d77e13f2f7d67c5560a636c7c962cd69e93
Enes Kuralay <kuralayenes@gmail.com>
2020-04-04 20:19:24 +0300

    denemeSon

---
+++ Object Oriented Programming/deneme1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  4 20:16:58 2020
+
+@author: eneskuralay
+"""
+
+
+
+
+pythonDeneme = "Hello World"
---
+++ Object Oriented Programming/test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 18 15:58:06 2019
+
+@author: eneskuralay
+"""
+
+a = 2+2 
+
+b = [1,2,3,4,5]
+
+c = [1,1,1,1,1]
+
+d = [2,3,4,5]
